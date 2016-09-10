6d3db0a35b875f32d1261ba15536a178ace867a4
Daniel Hrynczenko <daniel@phu.se>
2016-09-10 15:44:43 +0200

    added button example

---
+++ helmet/examples/buttocks.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+from neopixel import *
+
+
+# LED strip configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+
+#adjust for where your switch is connected
+buttonPin = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buttonPin,GPIO.IN)
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+	"""Movie theater light style chaser animation."""
+	for j in range(iterations):
+		for q in range(3):
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, color)
+			strip.show()
+			time.sleep(wait_ms/1000.0)
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, 0)
+
+def colorWipe(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	# Intialize the library (must be called once before other functions).
+strip.begin()
+
+while True:
+  #assuming the script to call is long enough we can ignore bouncing
+  while (GPIO.input(buttonPin)):
+        colorWipe(strip, Color(0, 0, 255)) # Green wipe
+  theaterChase(strip, Color(127, 0, 0)) # Red theater chase
