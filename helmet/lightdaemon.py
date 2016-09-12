38d369aa5c94c268165c067845e6bb1fd60340ef
Daniel Hrynczenko <daniel@phu.se>
2016-09-12 19:53:42 +0200

    initial version of lightdaemon

---
+++ helmet/lightdaemon.py
@@ -0,0 +1,50 @@
+import time
+from daemonize import Daemonize
+
+pid = "/tmp/test.pid"
+
+
+from neopixel import *
+
+
+# LED strip configuration:
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+	"""Movie theater light style chaser animation."""
+	for j in range(iterations):
+		for q in range(3):
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, color)
+			strip.show()
+			time.sleep(wait_ms/1000.0)
+			for i in range(0, strip.numPixels(), 3):
+				strip.setPixelColor(i+q, 0)
+def main():
+	# Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	# Intialize the library (must be called once before other functions).
+    strip.begin()
+    while True:
+		colorWipe(strip, Color(255, 0, 0))  # Red wipe
+		colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+		colorWipe(strip, Color(0, 0, 255))  # Green wipe
+
+
+daemon = Daemonize(app="test_app", pid=pid, action=main)
+daemon.start()
+
