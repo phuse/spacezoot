f6b88e2c0de8e8bd0bd8a6bd8e86dba6fe1d7c29
Daniel Hrynczenko <daniel@phu.se>
2016-09-13 22:21:43 +0200

    OTA updates!
    Quick and dirty OTA

--- helmet/netup.sh
+++ helmet/netup.sh
@@ -2,3 +2,5 @@
 mosquitto_pub -h localhost -m "connct" -t "helmet/netwrk"
 ip=`ifconfig wlan0 |grep "inet"|grep -v inet6| awk '{print $2}'`
 mosquitto_pub -h localhost -m $ip -t "helmet/IP"
+su -c "cd /home/pi/spacezoot;git pull" pi
+mosquitto_pub -h localhost -m $ip -t "helmet/IP"
